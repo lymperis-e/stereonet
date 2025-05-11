effb1a031a80efea45ad048037bda03f4eebdc7e
elymperis-desktop <geo.elymperis@gmail.com>
2025-05-11 23:51:33 +0300

    .

--- example/main.ts
+++ example/main.ts
@@ -1,21 +1,17 @@
 import { Stereonet } from "../src"; // Adjust the import path as necessary
 
-const streonet = new Stereonet({ selector: "body", size: 900, });
+const streonet = new Stereonet({ selector: "body", size: 900 });
 
 window.stereonet = streonet; // Expose the instance to the global scope for testing
 
 streonet.addPlane(30, 45); // Example usage
 streonet.addLine(30, 45); // Example usage
 
-
 streonet.addPlane(60, 90); // Example usage
-streonet.addLine(60, 90)
+streonet.addLine(60, 90);
 
+streonet.addPlane(83.2, 257);
+streonet.addLine(83.2, 257);
 
-streonet.addPlane(83.2, 257)
-streonet.addLine(83.2, 257)
-
-
-
-streonet.addLine(45, 336.6546)
-streonet.addPlane(336.6546, 45)
+streonet.addLine(45, 336.6546);
+streonet.addPlane(336.6546, 45);
--- src/core.ts
+++ src/core.ts
@@ -3,59 +3,59 @@ import * as d3 from "d3";
 
 import "./style.css";
 
-
 const DEFAULT_STYLE = {
-  "graticule": {
+  graticule: {
     fill: "none",
     stroke: " #777",
     "stroke-width": ".5px",
-    "stroke-opacity": .5,
+    "stroke-opacity": 0.5,
   },
-  "graticule_10_deg": {
+  graticule_10_deg: {
     stroke: "#000",
     "stroke-width": 0.6,
     fill: "none",
   },
-  "crosshairs": {
+  crosshairs: {
     stroke: "#000",
     "stroke-width": 1,
     fill: "none",
   },
-  "data_plane": {
+  data_plane: {
     stroke: "#F00",
     "stroke-width": 3,
-    fill: "none"
+    fill: "none",
     // fill: "#F00",
     // "fill-opacity": 0.5,
   },
-  "data_line": {
+  data_line: {
     fill: "#0328fc",
-    "stroke": "#0328fc",
+    stroke: "#0328fc",
     "stroke-width": 2,
     "stroke-opacity": 0.5,
     "fill-opacity": 1,
   },
-  "cardinal": {
+  cardinal: {
     fill: "#000",
     "font-size": "12px",
     "text-anchor": "middle",
   },
-}
+};
 
 interface StereonetOptions {
   selector: string;
   size?: number;
   style?: Record<string, Record<string, any>>;
-  animations?: {
-    duration: number;
-  } | false
+  animations?:
+    | {
+        duration: number;
+      }
+    | false;
 }
 
 /**
  * The D3 path type for rendered planes.
  */
 type PlanePath = d3.Selection<SVGPathElement, unknown, null, undefined>;
-
 
 /**
  * The D3 path type for rendered lines (lines are rendered as the points of their poles)
@@ -80,16 +80,22 @@ export class Stereonet {
   path: d3.GeoPath;
   cardinalValues: string[];
   styles: Record<string, Record<string, any>>;
-  animations: {
-    duration: number;
-  } | false;
+  animations:
+    | {
+        duration: number;
+      }
+    | false;
   planes: Map<string, PlanePath>;
   lines: Map<string, LinePath>;
 
-
-  constructor({ selector = "body", size = 1000, style = DEFAULT_STYLE, animations = {
-    duration: 300
-  } }: StereonetOptions) {
+  constructor({
+    selector = "body",
+    size = 1000,
+    style = DEFAULT_STYLE,
+    animations = {
+      duration: 300,
+    },
+  }: StereonetOptions) {
     this.width = size;
     this.height = size;
     this.selector = selector;
@@ -189,36 +195,50 @@ export class Stereonet {
       .datum(crosshairs)
       .attr("style", this.getStyle("crosshairs"))
       .attr("transform", `translate(${this.width / 2},${this.height / 2})`)
-      .attr("d", this.path)
-
+      .attr("d", this.path);
   }
 
   renderLabels() {
-    const graticule10 = d3.geoGraticule()
-      .extent([[-90, -90], [90.1, 90]])
+    const graticule10 = d3
+      .geoGraticule()
+      .extent([
+        [-90, -90],
+        [90.1, 90],
+      ])
       .step([10, 10])
       .precision(1);
 
-    this.g.selectAll('.graticule-label')
+    this.g
+      .selectAll(".graticule-label")
       .data(graticule10.lines())
-      .enter().append("text")
+      .enter()
+      .append("text")
       .attr("class", "graticule-label")
-      .text((d) => {
+      .text(d => {
         d.coordinates.reverse();
-        if (d.coordinates[0][0] === d.coordinates[1][0] && d.coordinates[0][0] >= 0) {
+        if (
+          d.coordinates[0][0] === d.coordinates[1][0] &&
+          d.coordinates[0][0] >= 0
+        ) {
           return this.reverseDegrees(d.coordinates[0][0]);
-        } else if (d.coordinates[0][1] === d.coordinates[1][1] && d.coordinates[0][1] !== -90) {
+        } else if (
+          d.coordinates[0][1] === d.coordinates[1][1] &&
+          d.coordinates[0][1] !== -90
+        ) {
           return this.reverseDegrees(d.coordinates[0][1]);
         }
       })
       .attr("dx", this.width / 3)
       .attr("dy", this.height / 3)
-      .attr("style", (d) =>
+      .attr("style", d =>
         this.reverseDegrees(d.coordinates[0][1]) > 90
           ? "alignment-baseline: hanging"
-          : "alignment-baseline: alphabetic")
-      .attr('transform', (d) =>
-        `translate(${this.projection(d.coordinates[1])[0]},${this.projection(d.coordinates[1])[1]})`
+          : "alignment-baseline: alphabetic"
+      )
+      .attr(
+        "transform",
+        d =>
+          `translate(${this.projection(d.coordinates[1])[0]},${this.projection(d.coordinates[1])[1]})`
       );
 
     this.g
@@ -243,12 +263,16 @@ export class Stereonet {
 
   private _validateDipDirection(dipAngle: number, dipDirection: number) {
     if (dipAngle < 0 || dipAngle > 90) {
-      console.warn(`Dip angle must be between 0 and 90 degrees (${dipAngle} provided). Skipping.`);
+      console.warn(
+        `Dip angle must be between 0 and 90 degrees (${dipAngle} provided). Skipping.`
+      );
       return false;
     }
 
     if (dipDirection < 0 || dipDirection > 360) {
-      console.warn(`Dip direction must be between 0 and 360 degrees (${dipDirection} provided). Skipping.`);
+      console.warn(
+        `Dip direction must be between 0 and 360 degrees (${dipDirection} provided). Skipping.`
+      );
       return false;
     }
 
@@ -282,8 +306,8 @@ export class Stereonet {
       if (!style) {
         throw new Error(`Style for class "${className}" not found.`);
       }
-      return style
-    }
+      return style;
+    };
 
     path
       .on("mouseover", function () {
@@ -298,24 +322,24 @@ export class Stereonet {
           .style("top", event.pageY + 10 + "px");
       })
       .on("mouseout", function () {
-        d3.select(this).style("stroke-width", getStyle("data_plane")["stroke-width"]); // Reset stroke width
+        d3.select(this).style(
+          "stroke-width",
+          getStyle("data_plane")["stroke-width"]
+        ); // Reset stroke width
         tooltip.style("display", "none");
       });
   }
-
 
   /**
    * Plots a line on the stereonet based on the given dip angle and dip direction.
    */
   addPlane(dipAngle: number, dipDirection: number) {
-
     // Validate the dip angle and dip direction
     if (!this._validateDipDirection(dipAngle, dipDirection)) {
       return;
     }
 
     const id = this.planes.size;
-
 
     const extentStart = 90 - dipAngle;
     const extentEnd = 90 - (dipAngle - 1);
@@ -338,11 +362,11 @@ export class Stereonet {
         `translate(${this.width / 2},${this.height / 2}) rotate(${dipDirection - 90})`
       )
       .attr("d", this.path)
-      .attr("data-id", id)
-
+      .attr("data-id", id);
 
     if (this.animations) {
-      path.style("opacity", 0) // Start with opacity 0 for animation
+      path
+        .style("opacity", 0) // Start with opacity 0 for animation
         .transition() // Add transition for animation
         .duration(300) // Animation duration in milliseconds
         .style("opacity", 1); // Fade in the plane
@@ -369,8 +393,6 @@ export class Stereonet {
     });
   }
 
-
-
   private _addLineHoverInteraction(
     path: d3.Selection<SVGPathElement, unknown, null, undefined>,
     dipAngle: number,
@@ -392,7 +414,7 @@ export class Stereonet {
 
     const tooltip = d3.select("#line-tooltip");
 
-    const classPath = this.path
+    const classPath = this.path;
 
     path
       .on("mouseover", function () {
@@ -433,8 +455,11 @@ export class Stereonet {
       .append("path")
       .datum(point)
       .attr("style", this.getStyle("data_line"))
-      .attr("transform", `translate(${this.width / 2},${this.height / 2}) rotate(${dipDirection})`)
-      .attr("data-id", id)
+      .attr(
+        "transform",
+        `translate(${this.width / 2},${this.height / 2}) rotate(${dipDirection})`
+      )
+      .attr("data-id", id);
 
     if (this.animations) {
       path
@@ -444,14 +469,11 @@ export class Stereonet {
         .duration(this.animations.duration) // Animation duration in milliseconds
         .attr("d", this.path.pointRadius(5))
         .style("opacity", 1); // Fade in the plane
-    }
-    else {
+    } else {
       path.attr("d", this.path.pointRadius(5));
     }
 
-
     this._addLineHoverInteraction(path, dipAngle, dipDirection);
-
 
     this.lines.set(id.toString(), path as LinePath);
     return id;
@@ -475,6 +497,4 @@ export class Stereonet {
   reverseDegrees(value: number) {
     return -1 * value + 90;
   }
-
-
 }
